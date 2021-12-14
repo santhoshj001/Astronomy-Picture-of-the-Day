3d67e2118fb365b58f445a18f07753353c16e178
santhoshj001 <santhoshj296@gmail.com>
2021-12-14 19:40:07 +0530

    hide API_Key with secrets-gradle-plugin

--- app/src/main/java/com/teamb/sj/apod/feature_home/data/remote/PictureDetailApi.kt
+++ app/src/main/java/com/teamb/sj/apod/feature_home/data/remote/PictureDetailApi.kt
@@ -1,5 +1,6 @@
 package com.teamb.sj.apod.feature_home.data.remote
 
+import com.teamb.sj.apod.BuildConfig
 import com.teamb.sj.apod.feature_home.data.remote.dto.PictureDetailDto
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,7 +9,7 @@ interface PictureDetailApi {
     companion object {
         val BASE_URL = "https://api.nasa.gov/"
         const val path = "planetary/apod"
-        const val API_KEY = ""
+        const val API_KEY = BuildConfig.API_KEY
     }
 
     @GET("$path?api_key=$API_KEY&thumbs=true")
