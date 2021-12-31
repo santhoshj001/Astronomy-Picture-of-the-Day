1f6b4fe9493eaa1a681972fb80c0d09c6bd7a624
santhoshj001 <santhoshj296@gmail.com>
2021-12-31 20:05:50 +0530

    refactoring App

--- app/src/main/java/com/teamb/sj/apod/feature_home/presentation/picturedetail/components/ExpandableText.kt
+++ app/src/main/java/com/teamb/sj/apod/feature_home/presentation/picturedetail/components/ExpandableText.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextLayoutResult
@@ -71,5 +76,4 @@ fun ExpandableText(
                 .animateContentSize(),
         )
     }
-
 }
